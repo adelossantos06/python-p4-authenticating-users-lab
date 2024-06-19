979f27c14fd945f9dd01938028dcf88ecaf5c9f0
Alyssa De Los Santos <adelossantos6@gmail.com>
2024-06-19 17:06:44 -0500

    complete authenticating users lab

--- server/app.py
+++ server/app.py
@@ -18,12 +18,42 @@ db.init_app(app)
 
 api = Api(app)
 
+class Login(Resource):
+    def post(self):
+        user = User.query.filter_by(username=request.json['username']).first()
+
+        if user:
+            session['user_id'] = user.id
+            return jsonify(user.to_dict())
+        else:
+            return jsonify({'message': 'User not found'}), 404
+
+api.add_resource(Login, '/login')
+
+class Logout(Resource):
+    def delete(self):
+        session['user_id'] = None
+        return {'message': '204: No Content'}, 204
+
+api.add_resource(Logout, '/logout')
+
+class CheckSession(Resource):
+    def get(self):
+        user = User.query.filter(User.id == session.get('user_id')).first()
+        print(user)
+        if user:
+            return user.to_dict(), 200
+        else:
+            return  {}, 401
+        
+api.add_resource(CheckSession, '/check_session')
+
 class ClearSession(Resource):
 
     def delete(self):
     
-        session['page_views'] = None
-        session['user_id'] = None
+        session.pop('page_views', None)
+        session.pop('user_id', None)
 
         return {}, 204
 
@@ -42,11 +72,13 @@ class ShowArticle(Resource):
         if session['page_views'] <= 3:
 
             article = Article.query.filter(Article.id == id).first()
-            article_json = jsonify(article.to_dict())
+            if article:
+                return jsonify(article.to_dict()), 200
+            else:
+                return jsonify({'message': 'Article not found'}), 404
+        else:
+            return jsonify({'message': 'Maximum pageview limit reached'}), 401
 
-            return make_response(article_json, 200)
-
-        return {'message': 'Maximum pageview limit reached'}, 401
 
 api.add_resource(ClearSession, '/clear')
 api.add_resource(IndexArticle, '/articles')
